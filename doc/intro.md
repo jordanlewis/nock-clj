136b25bbb02fa0992876308fc975a836c75f8fbd
Jordan Lewis <jordan@knewton.com>
2013-10-19 12:52:42 -0400

    Initial project structure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nock-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/nock_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns nock-clj.core-test
+  (:require [clojure.test :refer :all]
+            [nock-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
