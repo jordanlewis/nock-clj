651470ee4642b5a9c3fd25508b732e3868da1d12
Jordan Lewis <jordan@knewton.com>
2013-10-20 02:16:00 -0400

    Mostly working Nock

--- src/nock_clj/core.clj
+++ src/nock_clj/core.clj
@@ -1,6 +1,69 @@
-(ns nock-clj.core)
+(ns nock-clj.core
+  (:use [clojure.core.match :only (match)]
+        [clojure.tools.logging :only (spy)]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare tar)
+(defn nock [noun]
+  (tar noun))
+
+(defn ncdr [noun]
+  (if (empty? (rest (rest noun))) (second noun) (rest noun)))
+(defn ncar [noun]
+  (if (empty? (rest noun)) (first noun) noun))
+
+(defn cell [& args]
+  (if
+    (empty? (rest args))
+    (first args)
+    (let [x (apply cell (rest args))]
+      (if (seq? x)
+        (conj x (first args))
+        (list (first args) x)))))
+
+;; turn a nock cell into a flattened list
+(defn eat [x]
+  (if (coll? x) (apply cell (map eat x)) x))
+
+; is the noun an atom?
+; ?
+(defn wut [noun] (if (number? noun) 1 0))
+
+; +
+(defn lus [noun] (if (wut noun) (inc noun) noun))
+
+; =
+(defn tis [noun] (if (= (first noun) (ncdr noun)) 0 1))
+
+; \
+(defn slot [noun]
+  (let [p (first noun)
+        q (ncdr noun)]
+  (cond
+    (= p 1) q
+    (= p 2) (first q)
+    (= p 3) (ncdr q)
+    (even? p) (slot (cell 2 (slot (cell (quot p 2) q))))
+    (odd? p) (slot (cell 3 (slot (cell (quot p 2) q)))))))
+
+(defn tar [noun]
+  (if (or (number? noun) (number? (ncdr noun)))
+    noun
+    (let [[a b & c] noun]
+      (cond
+        (list? b) (cell (tar (cell a (first b) (ncdr b)))
+                        (tar (cell a c)))
+        (= 0 b) (slot (cell (ncar c) a))
+        (= 1 b) (ncar c)
+        (= 2 b) (tar (cell (tar (cell a (first c))) (tar (cell a (ncdr c)))))
+        (= 3 b) (wut (tar (cell a c)))
+        (= 4 b) (lus (tar (cell a c)))
+        (= 5 b) (tis (tar (cell a c)))
+        (= 6 b) (tar (cell a 2 (cell 0 1) 2 (cell 1 (second c) (ncdr (rest c)))
+                     (cell 1 0) 2 (cell 1 2 3) (cell 1 0) 4 4 (first c)))
+        (= 7 b) (tar (cell a 2 (first c) 1 (ncdr c)))
+        (= 8 b) (tar (cell a 7 (cell (cell 7 (cell 0 1) (first c)) 0 1) (ncdr c)))
+        (= 9 b) (tar (cell a 7 (ncdr c) 2 (cell 0 1) 0 (first c)))
+        (= 10 b) (tar (if (list? (first c))
+                   (cell a 8 (ncdr (first c) 7 '(0 3) (ncdr c)))
+                   (cell a (ncdr c))))
+        :else noun))))
--- test/nock_clj/core_test.clj
+++ test/nock_clj/core_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [nock-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest slot-test
+  (testing "slot examples")
+    (is (= '((4 5) (6 14 15))   (slot '(1 (4 5) (6 14 15)))))
+    (is (= '(4 5)               (slot '(2 (4 5) (6 14 15)))))
+    (is (= '(6 14 15)           (slot '(3 (4 5) (6 14 15)))))
+    (is (= 4                    (slot '(4 (4 5) (6 14 15)))))
+    (is (= 5                    (slot '(5 (4 5) (6 14 15)))))
+    (is (= 6                    (slot '(6 (4 5) (6 14 15)))))
+    (is (= '(14 15)             (slot '(7 (4 5) (6 14 15))))))
+
+(deftest tar-test
+  (testing "tar examples")
+    (is (= '(14 15) (tar '(((4 5) (6 14 15)) 0 7))))
+    (is (= '(153 218) (tar '(42 1 153 218))))
+    (is (= '(153 218) (tar (eat [77 [2 [1 42] [1 1 153 218]]]))))
+
+    (is (= 57 (tar '(57 0 1))))
+    (is (= 19 (tar '((132 19) 0 3))))
+    (is (= 58 (tar '(57 4 0 1))))
+    (is (= 20 (tar '((132 19) 4 0 3))))
+    (is (= '(43 1) (tar (eat [42 [[4 0 1] [3 0 1]]]))))
+    (is (= 20 (tar (eat [[132 19] [10 37 [4 0 3]]]))))
+    (is (= 44 (tar (eat [42 [7 [4 0 1] [4 0 1]]]))))
+    (is (= '(43 42) (tar (eat [42 [8 [4 0 1] [0 1]]]))))
+    (is (= 43 (tar (eat [42 [8 [4 0 1] [4 0 3]]]))))
+    (is (= 43 (tar (eat [42 [6 [1 0] [4 0 1] [1 233]]]))))
+    (is (= 233 (tar (eat [42 [6 [1 1] [4 0 1] [1 233]]]))))
+  )
+
+(deftest dec-test
+  (testing "Decrement 42")
+    (is (= 41 (tar (eat [42 [8 [1 0] 8 [1 6 [5 [0 7] 4 0 6] [0 6] 9 2 [0 2] [4 0 6] 0 7] 9 2 0 1]])))))
+
+(run-tests)
