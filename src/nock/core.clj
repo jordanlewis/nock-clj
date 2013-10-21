315f5d29518559890c986238ba74ec336a766417
Jordan Lewis <jordan@knewton.com>
2013-10-20 22:07:29 -0400

    Rewrite using core.match! cool

--- src/nock/core.clj
+++ src/nock/core.clj
@@ -31,10 +31,10 @@
   (if (coll? v) (apply cell (map eat v)) v))
 
 (defn ncdr [noun]
-  (if (empty? (rest (rest noun))) (second noun) (rest noun)))
+  (if (seq (rest (rest noun))) (rest noun) (second noun)))
 
 (defn ncar [noun]
-  (if (empty? (rest noun)) (first noun) noun))
+  (if (seq (rest noun)) noun (first noun)))
 
 ;; NOCK IMPLEMENTATION
 ;;
@@ -57,39 +57,30 @@
 ; \
 ; The slot (or fas) operator indexes into a list like a binary tree.
 (defn slot [noun]
-  (let [p (first noun)
-        q (ncdr noun)]
-  (cond
-    (= p 1) q
-    (= p 2) (first q)
-    (= p 3) (ncdr q)
-    (even? p) (slot (cell 2 (slot (cell (quot p 2) q))))
-    (odd? p) (slot (cell 3 (slot (cell (quot p 2) q)))))))
+  (match [noun]
+    [([1 & a] :seq)] (ncar a)
+    [([2 a & b] :seq)] a
+    [([3 a & b] :seq)] (ncar b)
+    [([(a :guard even?) & b] :seq)] (slot (cell 2 (slot (cell (quot a 2) (ncar b)))))
+    [([(a :guard odd? ) & b] :seq)] (slot (cell 3 (slot (cell (quot a 2) (ncar b)))))))
 
 ; *
 ; The tar operator is Nock itself. It evaluates a noun.
 (defn tar [noun]
-  (if (or (number? noun) (number? (ncdr noun)))
-    noun
-    (let [[a b & c] noun]
-      (cond
-        (list? b) (cell (tar (cell a (first b) (ncdr b)))
-                        (tar (cell a c)))
-        (= 0 b) (slot (cell (ncar c) a))
-        (= 1 b) (ncar c)
-        (= 2 b) (tar (cell (tar (cell a (first c))) (tar (cell a (ncdr c)))))
-        (= 3 b) (wut (tar (cell a c)))
-        (= 4 b) (lus (tar (cell a c)))
-        (= 5 b) (tis (tar (cell a c)))
-        (= 6 b) (tar (cell a 2 (cell 0 1) 2 (cell 1 (second c) (ncdr (rest c)))
-                     (cell 1 0) 2 (cell 1 2 3) (cell 1 0) 4 4 (first c)))
-        (= 7 b) (tar (cell a 2 (first c) 1 (ncdr c)))
-        (= 8 b) (tar (cell a 7 (cell (cell 7 (cell 0 1) (first c)) 0 1) (ncdr c)))
-        (= 9 b) (tar (cell a 7 (ncdr c) 2 (cell 0 1) 0 (first c)))
-        (= 10 b) (tar (if (list? (first c))
-                   (cell a 8 (ncdr (first c) 7 '(0 3) (ncdr c)))
-                   (cell a (ncdr c))))
-        :else noun))))
+  (match [noun]
+    [([a ([b & c] :seq) & d] :seq)] (cell (tar (cell a b c)) (tar (cell a d)))
+    [([a 0 & b] :seq)] (slot (cell (ncar b) a))
+    [([a 1 & b] :seq)] (ncar b)
+    [([a 2 b & c] :seq)] (tar (cell (tar (cell a b)) (tar (cell a c))))
+    [([a 3 & b] :seq)] (wut (tar (cell a b)))
+    [([a 4 & b] :seq)] (lus (tar (cell a b)))
+    [([a 5 & b] :seq)] (tis (tar (cell a b)))
+    [([a 6 b c & d] :seq)] (tar (cell a 2 '(0 1) 2 (cell 1 c d) '(1 0) 2 '(1 2 3) '(1 0) 4 4 b))
+    [([a 7 b & c] :seq)] (tar (cell a 2 b 1 (ncar c)))
+    [([a 8 b & c] :seq)] (tar (cell a 7 (cell (cell 7 (cell 0 1) b) 0 1) c))
+    [([a 9 b & c] :seq)] (tar (cell a 7 c 2 (cell 0 1) 0 b))
+    [([a 10 ([b & c] :seq) & d] :seq)] (tar (cell a 8  c 7 (cell 0 3) d))
+    [([a 10 b & c] :seq)] (tar (cell a c))))
 
 ;; the main entry-point to Nock for users. Pass in a real-life Nock noun as a
 ;; Clojure literal: (nock [1 0 1]) -> 1
