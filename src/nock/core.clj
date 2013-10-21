d1b1d45c6f5afebe0960fb16cc7e87491c2a015e
Jordan Lewis <jordan@knewton.com>
2013-10-20 22:38:15 -0400

    Rename ncar -> buy (ha) and update docs

--- src/nock/core.clj
+++ src/nock/core.clj
@@ -6,11 +6,11 @@
 ;; Internally, we're going to treat Nock nouns as regular lists for efficiency.
 ;; We treat a noun like [1 [2 3]] as '(1 2 3), for instance. This introduces a
 ;; complication: we have to treat singleton lists different from all other
-;; lists. In Nock, the second element of a sel is just an atom. But in
-;; Clojure, the second element of list is the rest of the list - which for
-;; a list of 2 elements, a.k.a. a Nock sel, is a singleton list. So we have
-;; to introduce a couple of functions, ncdr and ncar, that detect and ignore
-;; singleton lists in the right way.
+;; lists. In Nock, the second element of a 2-element cell is just an atom. But
+;; in Clojure, the second element of a list is the rest of the list - which for
+;; a list of 2 elements, a.k.a. a Nock cell, is a singleton list. So we have to
+;; introduce a function, buy (to continue this tradition of obfuscation), that
+;; detects and unwraps singleton lists.
 
 ;; sel creates a cell from a list of nouns. It's right associative, like in
 ;; Nock, but instead of making a long nested list of sels, it makes a regular
@@ -24,18 +24,18 @@
         (conj x (first args))
         (list (first args) x)))))
 
+;; buy unwraps singleton seqs. We need to use this any time we're
+;; going to operate on a noun that's passed in as a list, unless the
+;; noun is the last argument of a call to (sel), which does this
+;; unwrapping for us automatically if necessary.
+;; e.g. (buy '(1)) -> 1, but (buy '(1 2)) -> '(1 2)
+(defn buy [noun]
+  (if (seq (rest noun)) noun (first noun)))
+
 ;; eat turns a Nock noun literal into our internal representation.
 ;; e.g. (eat [1 2 3]) -> '(1 2 3)
 (defn eat [v]
   (if (coll? v) (apply sel (map eat v)) v))
-
-;; ncar unwraps singleton seqs. We need to use this any time we're
-;; going to operate on a noun that's passed in as a list, unless the
-;; noun is the last argument of a call to (sel), which does this
-;; unwrapping for us automatically if necessary.
-;; e.g. (ncar '(1)) -> 1, but (ncar '(1 2)) -> '(1 2)
-(defn ncar [noun]
-  (if (seq (rest noun)) noun (first noun)))
 
 ;; NOCK IMPLEMENTATION
 ;;
@@ -44,7 +44,7 @@
 ;; given at http://www.urbit.org/2013/08/22/Chapter-2-nock.html
 
 ; ?
-; The wut operator checks whether a noun is an atom or a sel.
+; The wut operator checks whether a noun is an atom or a cell
 (defn wut [noun] (if (number? noun) 1 0))
 
 ; +
@@ -54,16 +54,16 @@
 ; =
 ; The tis operator checks for equality between two nouns.
 (defn tis [noun] (cond (number? noun) noun
-                       (= (first noun) (ncar (rest noun))) 0
+                       (= (first noun) (buy (rest noun))) 0
                        :else 1))
 
 ; \
 ; The slot (or fas) operator indexes into a list like a binary tree.
 (defn slot [noun]
   (match [noun]
-    [([1 & a] :seq)] (ncar a)
+    [([1 & a] :seq)] (buy a)
     [([2 a & b] :seq)] a
-    [([3 a & b] :seq)] (ncar b)
+    [([3 a & b] :seq)] (buy b)
     [([(a :guard even?) & b] :seq)] (slot (sel 2 (slot (sel (quot a 2) b))))
     [([(a :guard odd? ) & b] :seq)] (slot (sel 3 (slot (sel (quot a 2) b))))))
 
@@ -72,8 +72,8 @@
 (defn tar [noun]
   (match [noun]
     [([a ([b & c] :seq) & d] :seq)] (sel (tar (sel a b c)) (tar (sel a d)))
-    [([a 0 & b] :seq)] (slot (sel (ncar b) a))
-    [([a 1 & b] :seq)] (ncar b)
+    [([a 0 & b] :seq)] (slot (sel (buy b) a))
+    [([a 1 & b] :seq)] (buy b)
     [([a 2 b & c] :seq)] (tar (sel (tar (sel a b)) (tar (sel a c))))
     [([a 3 & b] :seq)] (wut (tar (sel a b)))
     [([a 4 & b] :seq)] (lus (tar (sel a b)))
